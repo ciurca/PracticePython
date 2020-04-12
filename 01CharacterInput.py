aa1f664649261aecf5d6b57d3ffaf08ea74d2015
ciurca <raduciurca@gmail.com>
2020-04-12 11:32:32 +0300

    CharactherInput

--- 01CharacterInput.py
+++ 01CharacterInput.py
@@ -1,1 +1,5 @@
-print('hello world')
+import datetime
+name = input("Hello. Enter your name: ")
+age = input(f"{name}, enter your age: ")
+year = datetime.datetime.now().year - int(age) + 100
+print(f"{name}, you'll be 100 in the year {year}")
