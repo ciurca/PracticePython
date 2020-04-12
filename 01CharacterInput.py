6969c5d4e6b06e02a65b350ef4a15b9dc9f8bf65
ciurca <raduciurca@gmail.com>
2020-04-12 11:56:30 +0300

    Insane

--- 01CharacterInput.py
+++ 01CharacterInput.py
@@ -3,3 +3,5 @@ name = input("Hello. Enter your name: ")
 age = input(f"{name}, enter your age: ")
 year = datetime.datetime.now().year - int(age) + 100
 print(f"{name}, you'll be 100 in the year {year}")
+num1 = int(input("Give me another number: "))
+print((f"\n{name}, you'll be 100 in the year {year}") * num1)
