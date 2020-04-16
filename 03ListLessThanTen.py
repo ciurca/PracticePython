9a5a477623b81a25acc260d877d35dee55296812
ciurca <raduciurca@gmail.com>
2020-04-16 10:47:43 +0300

    3rd exercise

---
+++ 03ListLessThanTen.py
@@ -0,0 +1,8 @@
+'''original exercise below
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+print([n for n in a if n < 5])
+'''
+#extras 2 and 3 below
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+num = int(input("Enter a number: "))
+print([n for n in a if n < num])
